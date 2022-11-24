f9cdab7a76f5bb8028e25c73832303be3f78cd87
sandeeppal270 <sandeeppal270@gmail.com>
2022-11-24 17:43:05 +0530

    first commit

--- script.js
+++ script.js
@@ -11,8 +11,39 @@ endDate.addEventListener('change',function(e){
    e.preventDefault();
    clearInterval(timeInterval);
    console.dir(this);
-   const endDate = new Date(this.value);
+   const endDateTemp = new Date(this.value);
+   startClock(endDateTemp);
 
 
 })
+function startClock(endTime){
+    function updateCounter(){
+      let t = timeRemaining(endTime);
+      console.log(t);
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = t.hours;
+      minutesSpan.innerHTML = t.minutes;
+      secondsSpan.innerHTML = t.seconds;
+      if(t.total<=0){
+        clearInterval(timeInterval);
+      }
+    }
+    updateCounter();
+    timeInterval = setInterval(updateCounter,1000);
+}
+function timeRemaining(endTime){
+    let t = Date.parse(endTime)-Date.parse(new Date());
+    let seconds = Math.floor((t/1000)%60);
+    let minutes = Math.floor((t/1000/60)%60);
+    let hours = Math.floor((t/(1000*60*60))%24);
+    let days = Math.floor(t/(1000*60*60*24));
+    return {
+        'total':t,
+        'days':days,
+        'hours':hours,
+        'minutes':minutes,
+        'seconds':seconds
+    }
+
+}
 // /console.log(endDate);
